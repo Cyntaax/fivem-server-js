02cbfb0b7b98794d4275129d9737c905d7d190cd
Cyntaax C <cyntaax@gmail.com>
2022-02-07 11:39:29 -0500

    chore: ghetto memo :hankey:

--- lib/src/web/state.d.ts
+++ lib/src/web/state.d.ts
@@ -1,18 +1,5 @@
 declare class CircleState {
-    circles: {
-        name: string;
-        className: string;
-        strokeDashOffset: string;
-        stroke: string;
-        strokeWidth: string;
-        strokeLineCap: string;
-        cx: string;
-        cy: string;
-        r: string;
-        fill: string;
-        circumference: number;
-        strokeDashArray: string[];
-    }[];
+    circles: any[];
     constructor();
 }
 declare const circles: CircleState;
--- src/web/components/status/index.tsx
+++ src/web/components/status/index.tsx
@@ -1,36 +1,115 @@
 import { useAtom } from "jotai";
-import { observer } from "mobx-react-lite";
-import { createRef, FC, useEffect, useReducer, useRef, useState } from "react";
+import { createRef, FC, useCallback, useEffect, useReducer, useRef, useState } from "react";
 import useEvents from "../../../../lib/hooks/useEvents";
 import { circles } from "../../state";
 
-const Status: FC = observer(() => {
-	const _circles = circles.circles;
-	const [a, b] = useState([]);
+const Status: FC = () => {
+	const [circles, setCircles] = useState([]);
 	const events = useEvents();
-	events.addHandler<{ health: number; energy: number; stamina: number }>("hud:status", (data) => {
-		console.log(`circles`, JSON.stringify(_circles));
-		Object.keys(data).forEach((v) => {
-			updateCircle(v, data[v as keyof typeof data]);
-		});
-	});
+	const [lastPacket, setLastPacket] = useState<any>({});
 
-	const updateCircle = (name: string, pct: number) => {
-		const idx = _circles.findIndex((v) => v.name === name);
-		if (idx === -1) {
-			console.log(`not found`, circles);
-			return;
-		}
+	const cb = useCallback(
+		(data: any) => {
+			console.log(`circles`, JSON.stringify(circles));
+			Object.keys(data).forEach((v) => {
+				updateCircle(circles, v, data[v as keyof typeof data]);
+			});
+		},
+		[circles]
+	);
 
-		const data = _circles[idx];
-		data.circumference = 2 * Math.PI * parseInt(data.r);
-		const offset = (data.circumference - (pct / 100) * data.circumference).toString();
-		data.strokeDashOffset = offset;
-		data.strokeDashArray = [data.circumference.toString(), data.circumference.toString()];
-		const newArr = [..._circles];
-		newArr[idx] = data;
-		circles.circles = [...newArr];
-	};
+	const updateCircle = useCallback(
+		(_circles: any[], name: string, pct: number) => {
+			const idx = _circles.findIndex((v) => v.name === name);
+			if (idx === -1) {
+				console.log(`not found`, _circles);
+				return;
+			}
+
+			const data = _circles[idx];
+			data.circumference = 2 * Math.PI * parseInt(data.r);
+			const offset = (data.circumference - (pct / 100) * data.circumference).toString();
+			data.strokeDashOffset = offset;
+			data.strokeDashArray = [data.circumference.toString(), data.circumference.toString()];
+			const newArr = [..._circles];
+			newArr[idx] = data;
+			setCircles([...newArr]);
+		},
+		[circles]
+	);
+
+	const ghettoMemo = useCallback(
+		(data) => {
+			cb(data);
+		},
+		[circles]
+	);
+
+	useEffect(() => {
+		console.log(`CONSTRUCTING THE THING`, JSON.stringify(circles));
+	}, [cb]);
+	events.addHandler<{ health: number; energy: number; stamina: number }>("hud:status", ghettoMemo);
+
+	useEffect(() => {
+		setCircles([
+			{
+				name: "health",
+				className: "progress-ring__hours",
+				strokeDashOffset: "0",
+				stroke: "#b71c1c",
+				strokeWidth: "20",
+				strokeLineCap: "round",
+				cx: "150",
+				cy: "150",
+				r: "90",
+				fill: "transparent",
+				circumference: 0,
+				strokeDashArray: ["0", "0"]
+			},
+			{
+				name: "energy",
+				className: "progress-ring__energy",
+				strokeDashOffset: "0",
+				stroke: "#ffb300",
+				strokeWidth: "20",
+				strokeLineCap: "round",
+				cx: "150",
+				cy: "150",
+				r: "69",
+				fill: "transparent",
+				circumference: 0,
+				strokeDashArray: ["0", "0"]
+			},
+			{
+				name: "stamina",
+				className: "progress-ring__energy",
+				strokeDashOffset: "0",
+				stroke: "#388e3c",
+				strokeWidth: "20",
+				strokeLineCap: "round",
+				cx: "150",
+				cy: "150",
+				r: "48",
+				fill: "transparent",
+				circumference: 0,
+				strokeDashArray: ["0", "0"]
+			},
+			{
+				name: "breath",
+				className: "progress-ring__energy",
+				strokeDashOffset: "0",
+				stroke: "#0099cc",
+				strokeWidth: "5",
+				strokeLineCap: "round",
+				cx: "150",
+				cy: "150",
+				r: "32",
+				fill: "transparent",
+				circumference: 0,
+				strokeDashArray: ["0", "0"]
+			}
+		]);
+	}, []);
 
 	return (
 		<div className="status">
@@ -39,7 +118,7 @@ const Status: FC = observer(() => {
 				<circle className="bg" stroke="#61E700" strokeWidth="20" cx="150" cy="150" r="69" fill="transparent" />
 				<circle className="bg" stroke="#28D0DB" strokeWidth="20" cx="150" cy="150" r="48" fill="transparent" />
 
-				{_circles.map((v, i) => {
+				{circles.map((v, i) => {
 					return (
 						<circle
 							key={i}
@@ -47,7 +126,7 @@ const Status: FC = observer(() => {
 							stroke={v.stroke}
 							strokeWidth={v.strokeWidth}
 							strokeDashoffset={v.strokeDashOffset}
-							strokeLinecap={v.strokeLineCap as "round"}
+							strokeLinecap={v.strokeLineCap}
 							cx={v.cx}
 							cy={v.cy}
 							fill={v.fill}
@@ -70,6 +149,6 @@ const Status: FC = observer(() => {
 			</svg>
 		</div>
 	);
-});
+};
 
 export default Status;
